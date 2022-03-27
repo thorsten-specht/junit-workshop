ec471d23b08b47304a6f87e4e02dfe9f8b9da1d8
thorsten.specht <specht.thorsten@googlemail.com>
2022-03-27 19:03:11 +0200

    Product Owner now can really add story object to sprint

--- exercise/src/com/workshop/team/ProductOwner.java
+++ exercise/src/com/workshop/team/ProductOwner.java
@@ -5,9 +5,10 @@ import com.workshop.story.Story;
 
 public class ProductOwner {
 
-    void putStoryInSprint(Story story, Sprint sprint) {
+    public void putStoryInSprint(Story story, Sprint sprint) {
         if(sprint.getVelocity()-sprint.getLoad() <= story.getPoints()) {
             sprint.setLoad(sprint.getLoad()+story.getPoints());
+            sprint.addStoryToSprint(story);
         }
     }
 }
