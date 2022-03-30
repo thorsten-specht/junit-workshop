63c8deefb287056502c538644b1dd74f2e48a968
thorsten.specht <specht.thorsten@googlemail.com>
2022-03-30 19:17:21 +0200

    Finish developer class

--- exercise/src/com/workshop/team/Developer.java
+++ exercise/src/com/workshop/team/Developer.java
@@ -1,6 +1,7 @@
 package com.workshop.team;
 
-import com.workshop.events.Sprint;
+import com.workshop.story.Status;
+import com.workshop.story.Story;
 
 public class Developer extends Engineer {
     // attributes
@@ -20,8 +21,20 @@ public class Developer extends Engineer {
         this.workingHoursToday = workingHoursToday;
     }
 
+    public void setHasStoryInProgress(boolean hasStoryInProgress) {
+        this.hasStoryInProgress = hasStoryInProgress;
+    }
+
     public boolean isHasStoryInProgress() {
         return hasStoryInProgress;
     }
 
+    public void takeStoryInProgress(Story story) {
+        if (!hasStoryInProgress) {
+            super.takeStoryInStatus(story, Status.IN_PROGRESS);
+            hasStoryInProgress = true;
+        } else {
+            System.out.println("Developer must not work on more than one story");
+        }
+    }
 }
--- exercise/src/com/workshop/team/DeveloperSpec.java
+++ exercise/src/com/workshop/team/DeveloperSpec.java
@@ -1,6 +1,12 @@
 package com.workshop.team;
 
+import com.workshop.story.Status;
+import com.workshop.story.Story;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeveloperSpec {
@@ -11,5 +17,33 @@ public class DeveloperSpec {
         boolean hasStoryInProgress = developer.isHasStoryInProgress();
 
         assertFalse(hasStoryInProgress);
+    }
+
+    @Test
+    void developerCanTakeStoryInProgress() {
+        Developer developer = new Developer();
+        Story story = new Story(5, "Some story");
+
+        assertFalse(developer.hasStoryInProgress);
+        developer.takeStoryInProgress(story);
+        assertTrue(developer.hasStoryInProgress);
+
+        assertEquals(Status.IN_PROGRESS, story.getStatus());
+    }
+
+    @Test
+    void developerCanTakeOnlyOneStoryInProgress() {
+        Developer developer = new Developer();
+        Story story = new Story(5, "Some story");
+        developer.setHasStoryInProgress(true);
+
+        // set OutputStream to get access to console output
+        final ByteArrayOutputStream sysOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(sysOut));
+
+        developer.takeStoryInProgress(story);
+
+        assertEquals(Status.OPEN, story.getStatus());
+        assertEquals("Developer must not work on more than one story\n", sysOut.toString());
     }
 }
